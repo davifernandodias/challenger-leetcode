e4241b24b3a22c1d68178d5c67c6ef7b3e83826d
Perkles <davifernandosousadias18@gmail.com>
2024-03-09 19:21:18 -0300

    ex easys - comissão de vendas

--- src/ex_eayse/test_main.py
+++ src/ex_eayse/test_main.py
@@ -1,16 +1,9 @@
-import math
-#TODO exercio retornar o valor do meio do array
-
-numero = 0.43
-arrendo = math.ceil(numero)
-
-
-#        0 1 2 3 4
-head1 = [1,2,3,4,5]
-head2 = [1,2,3,4,5,6]
-tamanho_arr = len(head1)
-
-
-arra_lenght = len(head2)
-mid = arra_lenght//2
-print(head2[mid:])
+class people:
+    def employee():
+        name = input()
+        salary = float(input())
+        sales_amount = ((float(input())*15)/100)+salary
+        return f'TOTAL = R$ {sales_amount:.2f}'
+        
+chama = people.employee()
+print(chama)
