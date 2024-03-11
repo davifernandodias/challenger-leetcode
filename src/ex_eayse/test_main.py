8257b0273cc33f6d6cb6326415468be3dfed0447
Perkles <davifernandosousadias18@gmail.com>
2024-03-10 21:46:48 -0300

    changer args

--- src/ex_eayse/test_main.py
+++ src/ex_eayse/test_main.py
@@ -1,9 +1,2 @@
-class people:
-    def employee():
-        name = input()
-        salary = float(input())
-        sales_amount = ((float(input())*15)/100)+salary
-        return f'TOTAL = R$ {sales_amount:.2f}'
-        
-chama = people.employee()
-print(chama)
+if __name__ == '__main__':
+    return True
